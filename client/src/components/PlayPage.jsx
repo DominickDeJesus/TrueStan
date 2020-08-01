35ec39ba5117c3fa6f60ce96af6fa0db8c4dc425
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2020-08-01 18:33:47 -0400

    added comments and placeholders

--- client/src/components/PlayPage.jsx
+++ client/src/components/PlayPage.jsx
@@ -1,48 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { Form } from 'react-bootstrap';
-import { Route } from 'react-router-dom';
+import { Form, Image } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
 //TODO: add mp3 player
-//TODO: Add a game logic function that handles
-
-//May need this to keep track of what round the user is in
-//const [round, setRound] = useState(1);
-
-//TODO: Write the main logic of the game
-
-//temp varriable for testing
 
 const PlayPage = (artistObj) => {
   const [round, setRound] = useState(1);
   const history = useHistory();
 
-  let song = {
+  //test values for game
+  artistObj = {
     artistName: 'Jack Johnson',
-    previewUrl:
-      'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/94/25/9c/94259c23-84ee-129d-709c-577186cbe211/mzaf_5653537699505456197.plus.aac.p.m4a',
-    trackName: 'Better Together'
+    trackArr: [
+      {
+        previewUrl:
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/94/25/9c/94259c23-84ee-129d-709c-577186cbe211/mzaf_5653537699505456197.plus.aac.p.m4a',
+        trackName: 'Better Together'
+      }
+    ]
   };
 
-  const getRandomSong = (artistSongArr) => {
-    //Todo: make it output a random song that has not been picked yet
-    return artistSongArr && 'Better Together';
-  };
+  let answer = artistObj.trackArr[0].trackName;
 
-  const isGuessCorrect = (usrGuess) => {
-    if (usrGuess.toLowerCase() === song.trackName.toLowerCase()) {
+  useEffect(() => {
+    console.log(artistObj);
+  }, []);
+
+  /**Sets the next round if correct and send to game over page if guess is wrong.
+   * @returns if the answers are the same
+   * @param {*} usrGuess
+   * @param {*} answer
+   */
+  const isGuessCorrect = (usrGuess, answer) => {
+    if (usrGuess.toLowerCase() === answer.toLowerCase()) {
       setRound(round + 1);
-      console.log(`guess is right! ${round}`);
+      console.log(`Guess is right! ${round}`);
       return true;
     } else {
       setRound(-1);
       history.push('/gameover');
-      console.log(`guess is wrong! ${round}`);
+      console.log(`Guess is wrong! ${round}`);
       return false;
     }
   };
 
-  const getTimeLimit = () => {
+  const getTimeLimit = (round) => {
     if (round === 1) {
       console.log('Time is set to 15');
       return 15;
@@ -58,27 +60,38 @@ const PlayPage = (artistObj) => {
     }
   };
 
-  //TODO: Write the code to play a song for the given amount of time
-  const setSong = (song, time) => {};
-
+  /**Handles the submit for the guess. The function will make a call to check the geuss
+   * and then set the game for the next round if guess was right.
+   * @param {*} event
+   */
   const handleSubmit = (event) => {
     event.preventDefault();
+
     let guess = event.target.elements.searchbar.value;
     console.log(guess);
-    isGuessCorrect(guess);
-    setSong(getRandomSong(), getTimeLimit());
+
+    isGuessCorrect(guess, answer);
+    setSong(getRandomSong(), getTimeLimit(round));
     event.target.elements.searchbar.value = '';
   };
 
-  useEffect(() => {
-    console.log('I entered useEffect. Did it work?');
-    getRandomSong();
-  }, []);
+  //TODO: Write the code to play a song for the given amount of time
+  const setSong = (song, time) => {};
+
+  //FIX: function to initialize the player for the game
+  const getRandomSong = (artistSongArr) => {
+    //Todo: make it output a random song that has not been picked yet
+    return artistSongArr && 'Better Together';
+  };
 
   return (
     <div>
       <div>PlayPage</div>
       <h1>Round {round}</h1>
+      <Image
+        src="http://www.pngmart.com/files/4/Circle-Transparent-PNG.png"
+        style={{ width: 300 }}
+      ></Image>
       <Form onSubmit={handleSubmit}>
         <Form.Row>
           <Form.Control
