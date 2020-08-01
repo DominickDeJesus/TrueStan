1028e2c1d9039fd8cb390bf6d5f9fa4fdda8be7d
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2020-07-31 21:36:09 -0400

    added landing page js file

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//the landing page for the song game
+
+//Todo: make api request function
+//Todo: create landing page React component
+//Todo: Add searchbar react component
+
+const LandingPage = () => {};
+
+export default LandingPage;
