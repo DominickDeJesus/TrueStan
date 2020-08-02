016f4b870d3f2f0c18250521f8856e34d50fb5b7
zmminister <myles.ebusiness@gmail.com>
2020-08-01 20:58:33 -0400

    added player w/sample

--- client/src/components/PlayPage.jsx
+++ client/src/components/PlayPage.jsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+//import 'react-h5-audio-player/lib/styles.less'
+// import 'react-h5-audio-player/src/styles.scss' Use SASS
 
 //TODO: add mp3 player
 //TODO: Add a game logic function that handles
@@ -7,13 +12,21 @@ import React, { useState, useEffect } from 'react';
 //const [round, setRound] = useState(1);
 
 //TODO: Write the main logic of the game
-function playGame() {}
+//function playGame() {}
 
 //TODO: Write the code to play a song for the given amount of time
-function playSong(song, time) {}
+//function playSong(song, time) {}
 
 const PlayPage = () => {
-  return <div>PlayPage</div>;
+  return <div>
+    <img src= "https://i.postimg.cc/zB9wLNqM/Pell.jpg"></img>
+  <AudioPlayer
+    src= "https://www.mboxdrive.com/Pell-Show Out.mp3"
+    onPlay={e => console.log("onPlay")}
+    // other props here
+  />
+  
+  </div>;
 };
 
 export default PlayPage;
